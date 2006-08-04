7c28f9c078a93af461c13849faa6eaa45cf276bb
Robert M. Fuhrer <rmfuhrer@optonline.net>
2006-08-04 21:51:02 +0000

    Constructor now takes a file-name extension argument, to be used in determining the builder arguments via new base-class method getBuilderArguments().

--- src/org/eclipse/imp/smapifier/builder/SmapiProjectNature.java
+++ src/org/eclipse/imp/smapifier/builder/SmapiProjectNature.java
@@ -1,12 +1,19 @@
 package com.ibm.watson.smapifier.builder;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.uide.core.ProjectNatureBase;
 import org.eclipse.uide.runtime.IPluginLog;
-
 import com.ibm.watson.smapifier.SmapiePlugin;
 
 public class SmapiProjectNature extends ProjectNatureBase {
 	public static final String k_natureID= SmapiePlugin.kPluginID + ".smapinature";
+	private final String fExtension;
+
+	public SmapiProjectNature(String exten) {
+	    fExtension= exten;
+	}
 
 	public String getNatureID() {
 		return k_natureID;
@@ -21,11 +28,16 @@ public class SmapiProjectNature extends ProjectNatureBase {
 	}
 
 	protected void refreshPrefs() {
-		
-
 	}
 
 	protected String getUpstreamBuilderID() {
 		return "org.eclipse.jdt.core.javabuilder";
+	}
+
+	protected Map getBuilderArguments() {
+	    Map m= new HashMap();
+
+	    m.put("exten", fExtension);
+	    return m;
 	}
 }
