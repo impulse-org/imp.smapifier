977df293ba21d026aeebf892430ea9d018b47abb
Robert M. Fuhrer <rmfuhrer@optonline.net>
2006-08-04 21:39:51 +0000

    Accommodate change to SmapiProjectNature, whose ctor now requires a file-name extension argument (to identify what kind of files to process). For now, hard-wired to X10; work needs to be done to make it properly generic.

--- src/org/eclipse/imp/smapifier/actions/SmapifyAction.java
+++ src/org/eclipse/imp/smapifier/actions/SmapifyAction.java
@@ -10,17 +10,28 @@ import org.eclipse.ui.IActionDelegate;
 
 import com.ibm.watson.smapifier.builder.SmapiProjectNature;
 
-
+/**
+ * Action to enable the SMAP builder on the currently-selected project.<br>
+ * RMF 8/4/2006 --
+ * This looks generic, but at the moment is hard-wired in a trivial way for X10.
+ * See the associated action definition in plugin.xml, whose label makes that obvious.
+ * Previously, the SMAP builder was generic, and relied on builder arguments (defined
+ * in the .project) to specify the file-name extension of the files to process. Noone
+ * was specifying builder arguments, and the builder defaulted to X10, so it worked.
+ * Now the SMAP nature ctor takes a file-name extension argument, but we only have
+ * this one action to manually enable the SMAP builder on a project. Until such time
+ * as we enhance the action to ask what files to process, we leave it hard-wired for
+ * X10, which is the only language prepared to actually generate what SMAPI needs.
+ */
 public class SmapifyAction extends Action implements IActionDelegate {
-    private IProject fProject;
-    
-	public SmapifyAction(){
-		
+	private IProject fProject;
+
+	public SmapifyAction() {
 	}
 
 	public void run(IAction action) {
-		new SmapiProjectNature().addToProject(fProject);
-		
+		// RMF To make this generic, really need to pick up the file extension from somewhere... perhaps post a dialog?
+		new SmapiProjectNature("x10").addToProject(fProject);
 	}
 
 	public void selectionChanged(IAction action, ISelection selection) {
